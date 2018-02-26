1764aee288972492d8b85b17a5ed26671b7063ad
SyamilDz <mdsyamil9@gmail.com>
2018-02-26 23:01:11 +0100

    update frame line

--- fyp/src/fyp/colorswitch/entity/obstacle/frames/Line.java
+++ fyp/src/fyp/colorswitch/entity/obstacle/frames/Line.java
@@ -1,22 +1,33 @@
 package fyp.colorswitch.entity.obstacle.frames;
 
 import java.awt.Graphics2D;
+import java.awt.Shape;
 import java.awt.geom.Ellipse2D.Double;
 
 import fyp.colorswitch.Handler;
+import fyp.colorswitch.entity.obstacle.Obstacle;
 
-public class Line {
+public class Line extends Obstacle{
 	
 	private int color;
 	private float xStart, xEnd, yStart, yEnd;
 	private int i = 0;
 	
-	public Line(float xStart, float yStart, float xEnd, float yEnd, int color) {
+	private int ha1, ha2, ha3, ha4;
+	private int hb1, hb2, hb3, hb4;
+	
+	public Line(Handler handler, float yPosition, float xStart, float yStart, float xEnd, float yEnd, int color) {
+		super(handler, yPosition);
 		this.xStart = xStart;
 		this.yStart = yStart;
 		this.xEnd = xEnd;
 		this.yEnd = yEnd;
 		this.color = color;
+		
+		ha1 = 200; hb1 = 350;
+		ha2 = 350; hb2 = 350;
+		ha3 = 350; hb3 = 500;
+		ha4 = 350; hb4 = 350;
 	}
 
 	
@@ -81,6 +92,13 @@ public class Line {
 
 	public void setyEnd(float yEnd) {
 		this.yEnd = yEnd;
+	}
+
+
+	@Override
+	public boolean collidesWith(Shape body, int bodycolor) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
